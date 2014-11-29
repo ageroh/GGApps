71def84321fc4883e6033faaa111fa16f001b7ad
Argiris Gerogiannis <argigero@gmail.com>
2014-11-29 14:47:24 +0200

    correct version

---
+++ GGApps/Default.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GGApps._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+
+
+        <asp:LoginView ID="LoginView1" runat="server" >
+        <AnonymousTemplate>
+            <section class="featured">
+                <div class="content-wrapper">
+                    <hgroup class="title">
+                        <h1><%: Title %>.</h1>
+                        <h2>Please connect in order to move with app update for GreekGuide.</h2>
+                    </hgroup>
+                    <p>
+                                       
+                    </p>
+                </div>
+            </section>
+        </AnonymousTemplate>
+        <LoggedInTemplate>
+            <section class="featured">
+                <div class="content-wrapper">
+                    <hgroup class="title">
+                        <h2>Select the App to update for GreekGuide</h2>
+                    </hgroup>
+                </div>
+            </section>
+        </LoggedInTemplate>
+    </asp:LoginView>
+</asp:Content>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+
+    <asp:LoginView ID="LoginViewImportant" runat="server" >
+        <AnonymousTemplate>
+            <h3>Please first login!</h3>
+        </AnonymousTemplate>
+        <LoggedInTemplate>
+            <h3>Select application to update (one at a time)</h3>
+            <ol class="round">
+                <li class="one">
+                    <asp:DropDownList ID="ddStart" BackColor="GreenYellow" runat="server"></asp:DropDownList>
+                    <asp:Button id="GoFirst" runat="server" Text="Build Report" OnClick="GoFirst_Click"/>
+                </li>
+                <li class="two">
+                   Continue...
+                </li>
+            </ol>
+            <asp:ListView ID="MainResultScreen" runat="server">
+
+            </asp:ListView>
+        </LoggedInTemplate>
+    </asp:LoginView>
+</asp:Content>
+
+
+
+
+
+                        
+                   
---
+++ GGApps/Default.aspx.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Text;
+
+namespace GGApps
+{
+    public partial class _Default : Page
+    {
+
+        public static StringBuilder sb = new StringBuilder();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    DropDownList ddStart = (DropDownList)LoginViewImportant.FindControl("ddStart");
+
+                    ddStart.DataSource = FillDeptDropdownList();
+                    ddStart.DataTextField = "appName";
+                    ddStart.DataValueField = "id";
+                    ddStart.Items.Insert(0, " - Select App - ");
+
+                    ddStart.DataBind();
+                    
+                }
+            
+            }
+        }
+
+        public static DataTable FillDeptDropdownList()
+        {
+            System.Configuration.Configuration rootWebConfig1 = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("~");
+
+            if (rootWebConfig1.AppSettings.Settings["ContentAbilityGG"] != null)
+            {
+                SqlConnection con = new SqlConnection(rootWebConfig1.AppSettings.Settings["ContentAbilityGG"].Value.ToString());
+                SqlCommand cmd = new SqlCommand("select catCategoryId as id , catName as appName from category  where catParentID = 2", con);
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+
+                return dt;
+
+            }
+            return null;
+
+        }
+
+        public void GoFirst_Click(object sender, EventArgs e)
+        {
+            DropDownList ddStart = (DropDownList)LoginViewImportant.FindControl("ddStart");
+
+            if ( ! String.IsNullOrEmpty(ddStart.SelectedValue ))
+                Refresh_DB(Int32.Parse(ddStart.SelectedValue), ddStart.SelectedItem.ToString());
+            
+        }
+
+
+
+        void myConnection_InfoMessage(object sender, SqlInfoMessageEventArgs e)
+        {
+            sb.AppendLine("</br>" + e.Message);
+        }
+
+
+        public void Refresh_DB(int id, string name)
+        {
+            sb.Clear();
+
+            sb.AppendLine("</br>Refresing for Lang: 1");
+            sb.AppendLine("</br>");
+
+            string connectionString = BuildDynamicConnectionStringForDB(id, name, 1);
+             
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.InfoMessage += new SqlInfoMessageEventHandler(myConnection_InfoMessage);
+                con.Open();
+                using (SqlCommand command = new SqlCommand("_pr_refresh_db", con))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandTimeout = 1000;
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Log this information 
+                            Response.Write("</br>" + Convert.ToString(reader.GetValue(0)) + " = " + Convert.ToString(reader.GetValue(1)));
+                        }
+
+                    }
+                }
+            }
+
+            sb.AppendLine("</br>");
+            sb.AppendLine("</br>Refresing for Lang: 2");
+            sb.AppendLine("</br>");
+
+            connectionString = BuildDynamicConnectionStringForDB(id, name, 2);
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.InfoMessage += new SqlInfoMessageEventHandler(myConnection_InfoMessage);
+                con.Open();
+                using (SqlCommand command = new SqlCommand("_pr_refresh_db", con))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandTimeout = 1000;
+                    
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Log this information 
+                            Response.Write("</br>" + Convert.ToString(reader.GetValue(0)) + " = " + Convert.ToString(reader.GetValue(1)));
+                        }
+
+                    }
+
+                }
+            }
+
+            Response.Write(sb.ToString());
+
+
+        }
+
+
+
+        private string BuildDynamicConnectionStringForDB(int id, string name, int lang)
+        {
+             System.Configuration.Configuration rootWebConfig1 = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("~");
+             if (rootWebConfig1.AppSettings.Settings["DynamicConnectionString"] != null)
+             {
+                string dbName = "ContentDB_165_Lan_"+lang.ToString()+"_Cat_"+id.ToString();
+                string ConStr = rootWebConfig1.AppSettings.Settings["DynamicConnectionString"].Value;
+                ConStr = ConStr.Replace("DBNAME", dbName);
+                return ConStr;
+             }
+
+            return null;
+
+        }
+
+    }
+}
